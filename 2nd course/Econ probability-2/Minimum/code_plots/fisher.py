b14d88cae1070544ff63795bf8c9165694bfa44d
vinerdanya <vinerdanya@gmail.com>
2025-02-19 14:54:56 +0300

    upd min3

--- 2nd course/Econ probability-2/Minimum/code_plots/fisher.py
+++ 2nd course/Econ probability-2/Minimum/code_plots/fisher.py
@@ -2,14 +2,12 @@ from scipy.stats import f
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Выбираем параметры степеней свободы (m1, m2)
 df_pairs = [(1, 5), (5, 5), (10, 10), (20, 10)]
 
-x = np.linspace(0, 5, 1000)  # Определяем диапазон x
+x = np.linspace(0, 5, 1000)
 
 plt.figure(figsize=(8, 5))
 
-# Строим графики для F-распределения
 for k, m in df_pairs:
     plt.plot(x, f.pdf(x, k, m), label=fr'$k={k}, m={m}$')
 
